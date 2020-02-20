60f1a4e3e9667e36a65eaeca1e99a5dfed08ad8c
salemalem <wymedia@protonmail.com>
2020-02-20 11:10:13 +0600

    icon has been set.

--- app/src/main/java/com/example/widgetxmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/widgetxmusicplayer/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
+        //
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
